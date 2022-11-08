8e393474f8cad379f44216c8c75dc50590ab7dc8
Santiago Mondragón Gómez <santiago.mondragon@mercadolibre.com.co>
2022-11-07 22:39:08 -0500

    implement schedule fetching

--- src/api/rest.js
+++ src/api/rest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 export const cronos = axios.create({
   baseURL: 'https://ambrosia-cronos-ag-4axjffbidq-uc.a.run.app',
-  timeout: 20000,
+  timeout: 2000,
   headers: { 'X-Custom-Header': 'foobar' }
 });
 
--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -38,13 +38,15 @@ const events = [
   },
 ];
 
+const weekdays = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
 const Calendar = () => {
   const [userCourses, setUserCourses] = useState([]);
 
   useEffect(() => {
     cronos.post('/graphql', {
       query: `query {
-        UserCourses(userCode: "12345"){
+        UserCourses(userCode: "67890"){
           courseCode
           groupCode
           name   
@@ -64,14 +66,20 @@ const Calendar = () => {
       }`,
       variables: {}
     }).then((response) => {
+      console.log(response)
       setUserCourses(response.data.data.UserCourses);
-    }).catch(() => {
-      return [];
+    }).catch((err) => {
+      console.log(err)
     });
   }, []);
 
+  useEffect(() => {
+    if (userCourses.length !== 0) {
+      console.log(userCourses)
+    }
+  }, [userCourses]);
 
-  return (
+  return userCourses.length !== 0 ? (
     <div className="calendar-container">
       <FullCalendar
         locale={esLocale}
@@ -85,9 +93,36 @@ const Calendar = () => {
           minute: '2-digit',
           meridiem: false
         }}
-        events={events}
+        events={userCourses.map((course) => (
+          {
+            groupId: '1', // recurrent events in this group move together
+            daysOfWeek: course.schedules.map((schedule) => (
+              weekdays.indexOf(schedule.day).toString()
+            )),
+            startTime: course.schedules[0].timeOfStart,
+            endTime: course.schedules[0].timeOfEnd,
+            startRecur: '2022-08-08T12:00:00-05:00',
+            endRecur: '2022-12-08T12:00:00-05:00',
+            title: course.name
+          }
+        ))}
         hiddenDays={[0]}
       />
+    </div>
+  ) : (
+    <div className="grid h-screen place-items-center">
+      <div role="status">
+        <svg aria-hidden="true" className="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-emerald-600 fill-white"
+             viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"/>
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"/>
+        </svg>
+        <span className="sr-only">Loading...</span>
+      </div>
     </div>
   );
 };
