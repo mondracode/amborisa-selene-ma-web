407f90171feefb4880834871cfd975b9968a7e9d
Santiago Mondragón Gómez <santiago.mondragon@mercadolibre.com.co>
2022-11-14 18:14:01 -0500

    implement cors inverse proxy

--- src/api/rest.js
+++ src/api/rest.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const cronos = axios.create({
-  baseURL: 'https://ambrosia-cronos-ag-4axjffbidq-uc.a.run.app',
+  baseURL: 'https://ambrosia-cors-proxy-qokytbx7hq-uc.a.run.app/https://ambrosia-cronos-ag-4axjffbidq-uc.a.run.app',
   timeout: 2000,
   headers: { 'X-Custom-Header': 'foobar' }
 });
